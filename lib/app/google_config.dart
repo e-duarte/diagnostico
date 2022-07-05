b0b1d40f29968e3b9e0618ef66884515975f6730
Ewerton Duarte <30278417+e-duarte@users.noreply.github.com>
2022-07-05 13:02:46 -0300

    Update google_config.dart

--- lib/app/google_config.dart
+++ lib/app/google_config.dart
@@ -1,18 +1,18 @@
 class GoogleConfigurations {
   final credentials = r'''
 {
-  "type": "service_account",
-  "project_id": "rare-deployment-332518",
-  "private_key_id": "8cfb84c3b66b3140aae745a494225da2e07d7d20",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC8wYFrtUHMpe5O\n8j/twFWQlox1ly5UaCGRD2NHr+bsV6XIBQdNJqsCQfRi0u1QiF59yregYHYH7RZI\n0wdq/xdQMC26vi2p+6yGAXQq/X7z07an6a/cC4nMJeeTbLSDHrrjIR0Q93L6XNcU\nLgr9hpUmuagEeiG4Z4e71suoWsYv2f3uBoLj5KFrn7+oMuGme4iS1AHD3NsFDJ0f\nCJoD/ZnRBm4ekh2/WZl63uj3Mx9jJdJnGFqB3heqv19QUF7PxAV4KwNFDu2nha4Z\nIQQLm3I3vQeKnx2QGrZ11OzBtDaryigaWmPDjMFdabpl8a9m5XGow3PIm1UAiAgc\n2J9CTzNhAgMBAAECggEABO0oHAoDMHij0vc4MVvg0dMXkbO9QphEiMIoBjdcW5aU\np98fegFKcx7Kfowss+J4xJ/GbdDpsLaUq2g4Vj/X1XS581DejUxJfBaAcCUtR6bq\n0mhnQdMCZNfYCsdCcXYDRL186jAKpfeXiJYUbOoy5SuxG6OOQu9PDeFG78cVe0sB\ntDlBJzTsTv7hinzuYqkmbT65iEYjP6GIHHzIvyhffN1kXFUTO9GmXejc6xIPfcvK\nLxhZJxvioQeuctq1Bfnte+gW22ie/UC66G0TP9phNX8JVAZDmlT4ImwJYmLAO5id\nnLxRN1RKDJdgupl+rb87rY1hKUxAvQoHGZAjtYjCgQKBgQD2wSpMfpzhMXSRCkj8\n7jJuAtu0n4T8zB5h4DeL15KBskSuVPPR0Jz7mF4iiIpD1kJy4SsvQJP8eKIF5cr1\ndkrQr+kFPuKl9kVRjAjIwHZWZkowHCiD2iprK3qVK0Lm/S7/RZVG7ZTX8lFKlmR6\ncWTatJJJlgVmpZbNjRRP457OgQKBgQDD1AZ4OgZK+JAeC/4uz00DxctjTo1EgK6M\nqT/4U4KaU8QiK0t0pMlDib6SMwB/MKuRt1IpVtOQELdGcyoZuQr7kaa4c+25r+GE\n3hqt9U5N8Tmx9rFc6Id4w2S9foD4mVfnlbD+kAIBIVuxKFmShUGXXWYoLIcUoX+R\npQQzSAm04QKBgQCe1dSujMnbQ4RvYJl/Bq07l2dpq01llNbx1eyrfcZ26hO/zDtu\nkcSbR4dA33dxF1iKrlqn/f/Egrp45WRyAQVDtBhhIguvHQmd5nA5GXqTPyD+OwHx\nbdvkRIaRwtk76zn3ZAklMQWgxACD7K8dCiULizSnXYR4gJrEEc5DZpfxAQKBgHn3\nangjG5xCJ/Cu44dgEcWRwdV+LTpHM2/kUjVmPefE2Fm3wW1K3Re1iP4hyeNt+s3s\n8b1RmL8Xyi6Ut9oBiDebRc2IVr3aL4nDk7ZZHIYrAhY3YcxN/2JVwD7NtaIgT8Gr\nV+uU4cq2bhdu3CsH2AxgXaoT4aYV2BBsSKtv6KvBAoGAJtqBa+8/uUa4ui6KhCUy\nujoMfN4OTVfz2SGfL/RE8dsQKpo6ngtXUrTXzS1Jhr0jHUsiU1B8KnDWGCGmqoRM\nyq1h6hXud3e5D9e5qVFK123Uvxjbuf1sfkGFL46pak696CQPs/wAVKF4NxAUjU9P\n2dQ4nbcFdjN88NmiGnVxY0Y=\n-----END PRIVATE KEY-----\n",
-  "client_email": "mobilediagnostic@rare-deployment-332518.iam.gserviceaccount.com",
-  "client_id": "109010927391185167110",
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/mobilediagnostic%40rare-deployment-332518.iam.gserviceaccount.com"
+  "type": "",
+  "project_id": "",
+  "private_key_id": "",
+  "private_key": "",
+  "client_email": ""
+  "client_id": "",
+  "auth_uri": "",
+  "token_uri": "",
+  "auth_provider_x509_cert_url": "",
+  "client_x509_cert_url": ""
 }
 ''';
 
-  final spreadsheetMasterId = '1A8SIu2NFzHJ19c-PpMh8z4iYZiCwW6dn-VNRgCth5_w';
+  final spreadsheetMasterId = '';
 }
